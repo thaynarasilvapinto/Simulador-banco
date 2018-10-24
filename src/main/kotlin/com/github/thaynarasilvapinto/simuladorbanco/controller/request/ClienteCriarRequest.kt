bf8bd9b52acff274586433e38e3a7563227f1ec0
Thaynara Silva <thay.silva.ptc@gmail.com>
2018-10-24 14:06:58 -0300

    Adição da anotação para não permitir nome vazio

--- src/main/kotlin/com/github/thaynarasilvapinto/simuladorbanco/controller/request/ClienteCriarRequest.kt
+++ src/main/kotlin/com/github/thaynarasilvapinto/simuladorbanco/controller/request/ClienteCriarRequest.kt
@@ -1,10 +1,12 @@
 package com.github.thaynarasilvapinto.simuladorbanco.controller.request
 
 import org.hibernate.validator.constraints.br.CPF
+import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 data class ClienteCriarRequest(
         @get: NotNull
+        @get: NotEmpty
         val nome: String?,
         @get: NotNull
         @get: CPF
